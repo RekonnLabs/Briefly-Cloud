bc6d0cebc3d0eae704b3fd5228c49df1b2e77be5
RekonnLabs <rekonnlabs@gmail.com>
2025-09-04 16:40:27 -0600

    GooglePicker component

--- briefly-cloud-nextjs/src/app/providers.tsx
+++ briefly-cloud-nextjs/src/app/providers.tsx
@@ -1,7 +1,13 @@
 'use client'
 
 import { SupabaseAuthProvider } from '@/app/components/auth/SupabaseAuthProvider'
+import { Toaster } from 'sonner'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-	return <SupabaseAuthProvider>{children}</SupabaseAuthProvider>
+	return (
+		<SupabaseAuthProvider>
+			{children}
+			<Toaster richColors position="top-right" closeButton />
+		</SupabaseAuthProvider>
+	)
 }
