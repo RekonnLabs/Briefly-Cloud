4ba37ebbb12d12b2a2c6c94977fe74bb0de7bbda
RekonnLabs <rekonnlabs@gmail.com>
2025-10-22 12:48:49 -0600

    Privacy and Terms Updates

---
+++ briefly-cloud-nextjs/src/app/terms/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from 'next/navigation';
+
+export default function TermsPage() {
+  // Redirect to the main Rekonn Labs terms page
+  redirect('https://rekonnlabs.com/terms');
+}
